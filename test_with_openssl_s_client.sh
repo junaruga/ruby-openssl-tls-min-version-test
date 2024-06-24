69f9197920ff531ba037b7c937890a01ef588cb7
Jun Aruga <jaruga@redhat.com>
2024-06-24 15:11:43 +0200

    Run SSL servers by `openssl s_server` instead of HTTPD.
    Fix an bash error in the test_with_openssl_s_client.sh.
    
    * Add -debug and -trace options to the openssl s_server and s_client.
    * Remove TLS 1.0/1.1 cases.
      There are unknown issues on the cases.
      The remaining 2 cases TLS 1.2/1.3 cases are enough to verify the issue.

--- test_with_openssl_s_client.sh
+++ test_with_openssl_s_client.sh
@@ -3,18 +3,15 @@
 set -euxo pipefail
 
 DOMAIN='example.com'
-
-HOSTS="
-    tls-10.${DOMAIN}
-    tls-11.${DOMAIN}
-    tls-12.${DOMAIN}
-    tls-13.${DOMAIN}
+CONFIG_ITEMS="
+    tls-12.${DOMAIN}|44333
+    tls-13.${DOMAIN}|44334
 "
 CA_FILE='tmp/test.crt'
 LOG_DIR="log/openssl-s_client"
 
 OPENSSL_DIR="${OPENSSL_DIR:-}"
-if "${OPENSSL_DIR}" = ''; then
+if [ -z "${OPENSSL_DIR}" ]; then
     OPENSSL_CLI='openssl'
 else
     OPENSSL_CLI="${OPENSSL_DIR}/bin/openssl"
@@ -23,9 +20,14 @@ fi
 rm -rf "${LOG_DIR}"
 mkdir -p "${LOG_DIR}"
 
-for host in ${HOSTS}; do
-    if ! "${OPENSSL_CLI}" s_client -connect "${host}:443" \
+for item in ${CONFIG_ITEMS}; do
+    host="$(echo "${item}" | cut -d '|' -f 1)"
+    port="$(echo "${item}" | cut -d '|' -f 2)"
+
+    if ! "${OPENSSL_CLI}" s_client -connect "${host}:${port}" \
         -CAfile "${CA_FILE}" \
+        -debug \
+        -trace \
         < /dev/null \
         > "${LOG_DIR}/${host}.stdout.log" \
         2> "${LOG_DIR}/${host}.stderr.log"; then
