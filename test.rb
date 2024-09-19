15f8a3275364a3c3d201a53905abe9a61abbd0e3
Jun Aruga <jaruga@redhat.com>
2024-09-19 17:31:29 +0200

    Assert the tests in the case of upstream OpenSSL.

--- test.rb
+++ test.rb
@@ -7,9 +7,14 @@ CONFIG_ITEMS = {
   tls_1_3: { host: "tls-13.#{DOMAIN}", port: 44_334 }
 }.freeze
 CA_FILE = 'tmp/test.crt'
+# Is the used OpenSSL a downstream OpenSSL RPM?
+# false: Use Upstream OpenSSL.
+DOWNSTREAM_OPENSSL = true
 
 class TestRubyOpenSSLTLSMin < Test::Unit::TestCase
-  def test_with_tls_1_2_server
+  def test_connect_to_tls_1_2_server_on_downstream_openssl
+    omit unless DOWNSTREAM_OPENSSL
+
     # The connection to the TLS 1.2 server should fail.
     assert_raise(OpenSSL::SSL::SSLError) do
       connect_with_openssl_s_client(CONFIG_ITEMS[:tls_1_2][:host],
@@ -17,7 +22,25 @@ class TestRubyOpenSSLTLSMin < Test::Unit::TestCase
     end
   end
 
-  def test_with_tls_1_3_server
+  def test_connect_to_tls_1_2_server_on_upstream_openssl
+    omit if DOWNSTREAM_OPENSSL
+
+    # The connection to the TLS 1.2 server should pass.
+    assert(connect_with_openssl_s_client(CONFIG_ITEMS[:tls_1_2][:host],
+                                         CONFIG_ITEMS[:tls_1_2][:port]))
+  end
+
+  def test_connect_to_tls_1_3_server_on_downstream_openssl
+    omit unless DOWNSTREAM_OPENSSL
+
+    # The connection to the TLS 1.3 server should pass.
+    assert(connect_with_openssl_s_client(CONFIG_ITEMS[:tls_1_3][:host],
+                                         CONFIG_ITEMS[:tls_1_3][:port]))
+  end
+
+  def test_connect_to_tls_1_3_server_on_upstream_openssl
+    omit if DOWNSTREAM_OPENSSL
+
     # The connection to the TLS 1.3 server should pass.
     assert(connect_with_openssl_s_client(CONFIG_ITEMS[:tls_1_3][:host],
                                          CONFIG_ITEMS[:tls_1_3][:port]))
@@ -25,8 +48,6 @@ class TestRubyOpenSSLTLSMin < Test::Unit::TestCase
 
   def connect_with_openssl_s_client(host, port)
     uri = URI("https://#{host}")
-    puts "host: #{host}"
-    puts "host: #{port}"
     Net::HTTP.start(host,
                     port,
                     use_ssl: true,
