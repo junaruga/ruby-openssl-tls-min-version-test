762a365da524884f8a1267ef761cb3390a0ceacb
Jun Aruga <jaruga@redhat.com>
2024-09-19 14:12:45 +0200

    Assert tests.

--- test.rb
+++ test.rb
@@ -1,28 +1,37 @@
 require 'net/http'
+require 'test/unit'
 
 DOMAIN = 'example.com'
-CONFIG_ITEMS = [
-  { host: "tls-12.#{DOMAIN}", port: 44_333 },
-  { host: "tls-13.#{DOMAIN}", port: 44_334 }
-].freeze
+CONFIG_ITEMS = {
+  tls_1_2: { host: "tls-12.#{DOMAIN}", port: 44_333 },
+  tls_1_3: { host: "tls-13.#{DOMAIN}", port: 44_334 }
+}.freeze
+CA_FILE = 'tmp/test.crt'
 
-ca_file = 'tmp/test.crt'
+class TestRubyOpenSSLTLSMin < Test::Unit::TestCase
+  def test_with_tls_1_2_server
+    # The connection to the TLS 1.2 server should fail.
+    assert_raise(OpenSSL::SSL::SSLError) do
+      connect_with_openssl_s_client(CONFIG_ITEMS[:tls_1_2][:host],
+                                    CONFIG_ITEMS[:tls_1_2][:port])
+    end
+  end
 
-CONFIG_ITEMS.each do |item|
-  # require 'debug'
-  # binding.break
+  def test_with_tls_1_3_server
+    # The connection to the TLS 1.3 server should pass.
+    assert(connect_with_openssl_s_client(CONFIG_ITEMS[:tls_1_3][:host],
+                                         CONFIG_ITEMS[:tls_1_3][:port]))
+  end
 
-  uri = URI("https://#{item[:host]}")
-  begin
-    Net::HTTP.start(item[:host],
-                    item[:port],
+  def connect_with_openssl_s_client(host, port)
+    uri = URI("https://#{host}")
+    puts "host: #{host}"
+    puts "host: #{port}"
+    Net::HTTP.start(host,
+                    port,
                     use_ssl: true,
-                    ca_file: ca_file) do |http|
+                    ca_file: CA_FILE) do |http|
       http.get(uri)
     end
-  rescue StandardError => e
-    puts "Failed to load from #{item[:host]}: #{e}"
-  else
-    puts "Loaded from #{item[:host]}"
   end
 end
--- test_with_openssl_s_client.sh
+++ test_with_openssl_s_client.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -euxo pipefail
+set -euo pipefail
 
 # Custom
 #
@@ -16,35 +16,56 @@ else
 fi
 # Domain.
 DOMAIN='example.com'
-
-CONFIG_ITEMS="
-    tls-12.${DOMAIN}|44333
-    tls-13.${DOMAIN}|44334
-"
+# SSL server settings
+TLS_1_2_HOST="tls-12.${DOMAIN}"
+TLS_1_3_HOST="tls-13.${DOMAIN}"
+TLS_1_2_PORT="44333"
+TLS_1_3_PORT="44334"
 CA_FILE='tmp/test.crt'
 LOG_DIR="log/openssl-s_client"
 OPENSSL_CLI="${OPENSSL_DIR}/bin/openssl"
 
-rm -rf "${LOG_DIR}"
-mkdir -p "${LOG_DIR}"
+function connect_with_openssl_s_client {
+    local host="${1}"
+    local port="${2}"
 
-if [ "${DOWNSTREAM_OPENSSL}" = true ]; then
-    grep '^TLS.Min' /etc/crypto-policies/back-ends/opensslcnf.config
-fi
-
-for item in ${CONFIG_ITEMS}; do
-    host="$(echo "${item}" | cut -d '|' -f 1)"
-    port="$(echo "${item}" | cut -d '|' -f 2)"
-
-    if ! "${OPENSSL_CLI}" s_client -connect "${host}:${port}" \
+    "${OPENSSL_CLI}" s_client -connect "${host}:${port}" \
         -CAfile "${CA_FILE}" \
         -debug \
         -trace \
         < /dev/null \
         > "${LOG_DIR}/${host}.stdout.log" \
-        2> "${LOG_DIR}/${host}.stderr.log"; then
-        echo "Failed from ${host}"
-    else
-        echo "Passed from ${host}"
+        2> "${LOG_DIR}/${host}.stderr.log"
+}
+
+rm -rf "${LOG_DIR}"
+mkdir -p "${LOG_DIR}"
+
+if [ "${DOWNSTREAM_OPENSSL}" = true ]; then
+    if ! grep -q '^TLS.MinProtocol = TLSv1.3$' \
+        /etc/crypto-policies/back-ends/opensslcnf.config; then
+        echo "[ERROR] The crypto-policies opensslcnf.config TLS.MinProtocol " \
+            "should be set as TLSv1.3 for testing." 1>&2
+        exit 1
     fi
-done
+fi
+
+# The connection to the TLS 1.2 server should fail.
+if ! connect_with_openssl_s_client "${TLS_1_2_HOST}" "${TLS_1_2_PORT}"; then
+    echo "TLS 1.2 test - OK"
+else
+    echo "The connection to the TLS 1.2 server should fail " \
+        "with the crypto-policies opensslcnf.config TLS.MinProtocol TLSv1.3." \
+        1>&2
+    exit 2
+fi
+
+# The connection to the TLS 1.3 server should pass.
+if connect_with_openssl_s_client "${TLS_1_3_HOST}" "${TLS_1_3_PORT}"; then
+    echo "TLS 1.3 test - OK"
+else
+    echo "The connection to the TLS 1.3 server should pass " \
+        "with the crypto-policies opensslcnf.config TLS.MinProtocol TLSv1.3." \
+        1>&2
+    exit 3
+fi
